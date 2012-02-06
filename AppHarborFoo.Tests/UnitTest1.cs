52828333a6be4dbfc6ec1258a6b7bde975deaf4a
unknown <Matthew@AMXVMBR.(none)>
2012-02-06 12:26:41 +0000

    Unit testing testing

---
+++ AppHarborFoo.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppHarborFoo.Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class MyTests
+    {
+        [TestMethod()]
+        public void TestFailure()
+        {
+            throw new InvalidOperationException("I'm designed to stop deployment!");
+        }
+    }
+}
--- AppHarborFoo/Foo.cs
+++ AppHarborFoo/Foo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 
 namespace AppHarborFoo
 {
@@ -9,7 +10,7 @@ namespace AppHarborFoo
     {
         public string DoMagic()
         {
-            return string.Format("Bar4 ... {0}", DateTime.Now);
+            return string.Format("Bar - {0} - {1}", DateTime.Now, ConfigurationManager.AppSettings["SQLSERVER_URI"]);
         }
     }
 }
