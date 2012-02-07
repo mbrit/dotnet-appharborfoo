039f2105a07aec79325114165724c90710dab9c6
unknown <Matthew@AMXVMBR.(none)>
2012-02-07 22:44:17 +0000

    Failing test

---
+++ AppHarborFoo.Tests/MyTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppHarborFoo.Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class MyTests
+    {
+        [TestMethod()]
+        public void TestFailure()
+        {
+            throw new InvalidOperationException("I'm designed to stop deployment!");
+        }
+
+        //[TestMethod()]
+        //public void TestSuccess()
+        //{
+        //    // ...whereas I'm designed to allow deployment!
+        //}
+    }
+}
