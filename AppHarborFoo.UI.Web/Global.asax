ba18df5b140fd1ae8ff0e9f4a395475f01c3ad65
unknown <Matthew@AMXVMBR.(none)>
2012-02-08 11:42:04 +0000

    Database update

---
+++ AppHarborFoo.UI.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppHarborFoo.UI.Web.Global" Language="C#" %>
--- AppHarborFoo/Database/DbHelper.cs
+++ AppHarborFoo/Database/DbHelper.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using Dapper;
 
 namespace AppHarborFoo
 {
-    internal static class DbHelper
+    public static class DbHelper
     {
         private static string _connectionString;
 
@@ -45,6 +46,28 @@ namespace AppHarborFoo
 
                 // return...
                 return _connectionString;
+            }
+        }
+
+        public static void UpdateDatabase()
+        {
+            using(IDbConnection conn = GetConnection())
+            {
+                // if we don't have a table called Customers, create one...
+                if (conn.Query<string>("select table_name from information_schema.tables where table_name='customers'", null).Count() == 0)
+                {
+                    // create...
+                    conn.Execute(@"CREATE TABLE [dbo].[Customers](
+	                    [CustomerId] [int] IDENTITY(1,1) NOT NULL,
+	                    [FirstName] [nvarchar](64) NOT NULL,
+	                    [LastName] [nvarchar](64) NOT NULL,
+	                    [Email] [nvarchar](64) NOT NULL,
+                     CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+                    (
+	                    [CustomerId] ASC
+                    )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+                    ) ON [PRIMARY]", null);
+                }
             }
         }
     }
